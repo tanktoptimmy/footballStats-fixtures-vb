c1f596461079a9436771d946bf81850e17a4f983
Timothy Parry <tim.parry@skybettingandgaming.com>
2023-11-17 11:14:07 +0000

    addinv save to mongodb

---
+++ models/fixturesSchema.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const fixtureSchema = new mongoose.Schema({
+  _id: Number,
+  league: Number,
+  date: Date,
+  teams: {
+    home: {
+      name: String,
+    },
+    away: {
+      name: String,
+    }
+  },
+  round: String,
+  league: {
+    name: String,
+    id: Number,
+  }
+})
+
+export const FixtureModel = mongoose.model('Fixture', fixtureSchema)
--- pages/api/vb.js
+++ pages/api/vb.js
@@ -46,29 +46,85 @@
 import Axios from 'axios'
 import mongoose from 'mongoose'
 
-import dbConnect from './utils/dbConnect.js'
-import { MatchModel } from './models/matchesSchema.js'
+import dbConnect from '@/utils/dbConnect.js'
+import { FixtureModel } from '@/models/fixturesSchema.js'
 
-export default async function main() {
+const makeRequest = async (league) => {
+  const options = {
+    method: 'GET',
+    url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures',
+    params: league,
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPIDAPIKEY,
+      'X-RapidAPI-Host': process.env.RAPIDHOST
+    }
+  };
+    try {
+      const response = await Axios(options);
+      return response.data; // Assuming the response contains the data you need
+    } catch (error) {
+      console.error(`Error fetching data for league ${league.league}:`, error);
+      return null;
+    }
+}
+
+const getFixtures = async () => {
+  const leagues = [
+    {
+      league: '39',
+      season: '2023'
+    }
+  ]
+
+  // Array to store all the promises for fetching data
+  const fetchPromises = [];
+
+  // Iterate over each leagueId and create Axios requests
+  leagues.forEach(league => {
+    const promise = makeRequest(league);
+    fetchPromises.push(promise);
+  });
+
+  try {
+  // Wait for all requests to complete
+  return await Promise.all(fetchPromises)
+  // .then(results => {
+  //   // 'results' will be an array containing the data from all the requests
+  //   // Merge/Process the data as needed
+  //   const mergedData = results.reduce((merged, data) => {
+  //     // Assuming you want to merge the data into a single object
+  //     return { ...merged, ...data };
+  //   }, {});
+
+  //   console.log('Merged Data:', mergedData);
+  // })
+  } catch(error) {
+    console.error('Error fetching league data:', error);
+    return null;
+  };
+}
+
+export default async function main(req, res) {
   // get all the fixtures
+  const fixtures = await getFixtures();
 
-  // filter out all the fixtures you want
-  // const filtered = response.filter(obj => {
-  //  return obj.league.round === "Regular Season - 1"
-  // });
+  // Extracting response arrays and merging them into a single array
+  const mergedFixtures = fixtures.reduce((accumulator, obj) => {
+  // Concatenate the response array of each object with the accumulator
+  return accumulator.concat(obj.response);
+}, []);
 
-  // create fixture strings i.e. 112323-123123-4323432-2345435 - can be up to 20 long
-  const fixtureString = filtered.map(obj => obj.fixture.id).join('-')
-  const fixtures = await getFixtures(fixtureString)
-  saveAllFixtures(fixtures)
+  saveAllFixtures(mergedFixtures)
     .then(() => {
       // Close the MongoDB connection after saving all fixtures
       mongoose.connection.close()
+      res.status(200).json({ message: "All saved nicely" });
     })
     .catch(error => {
       console.error('Error saving fixtures:', error.message)
       // Close the MongoDB connection in case of an error
       mongoose.connection.close()
+      res.status(400).json({ message: "Houston, we've had a problem" });
     })
 }
 
@@ -78,13 +134,22 @@ const saveFixture = async fixture => {
     await dbConnect()
     const newFixture = {
       _id: fixture.fixture.id,
-      ...fixture
+      date: fixture.fixture.date,
+      round: fixture.league.round,
+      league: {
+        name:fixture.league.name,
+        id: fixture.league.id,
+      },
+      teams: {
+        home: fixture.teams.home.name,
+        away: fixture.teams.away.name
+      }
     }
 
     const filter = { _id: fixture.fixture.id } // Assuming _id is present in the fixtureData
     const update = { $set: newFixture }
     const options = { upsert: true }
-    await MatchModel.updateOne(filter, update, options)
+    await FixtureModel.updateOne(filter, update, options)
   } catch (error) {
     throw new Error('Error saving fixture:', error.message)
   }
