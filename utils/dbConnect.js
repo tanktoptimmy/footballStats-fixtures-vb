fbb0a1efe371aa2bad7e40bf5032a9511db15aeb
Timothy Parry <tim.parry@skybettingandgaming.com>
2023-12-14 07:37:34 +0000

    adding leagues to console log

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -6,6 +6,12 @@ async function dbConnect() {
   try {
     await mongoose.connect(process.env.MONGODB_URI)
     console.log('Connected to MongoDB')
+    console.log(`39: Premier League
+    40: Championship
+    140: La Liga
+    78: Bundesliga
+    135: Serie A Italy
+    61: Ligue 1`)
   } catch (error) {
     console.error('Error connecting to MongoDB:', error)
   }
